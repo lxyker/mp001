ac57dcab0a10a36e11bb2b40380ef5739d8e7b73
lxyker <lxyker@qq.com>
2021-05-18 19:40:21 +0800

    2021-05-18 19:40 down work

--- src/main/java/com/lxyker/mp001/entity/User.java
+++ src/main/java/com/lxyker/mp001/entity/User.java
@@ -1,5 +1,7 @@
 package com.lxyker.mp001.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,10 @@ import java.math.BigInteger;
 
 @Data
 public class User {
+
+//    @TableId(type = IdType.AUTO)
     private Long id;
+
     private String name;
     private Integer age;
     private String email;
